dd65f64ccd0734cb0d6ad33cb8018b500ed187d8
Kaushik066 <masterkaushikrao@gmail.com>
2017-09-26 09:07:04 +0530

    Made some changes locally

--- app_main.js
+++ app_main.js
@@ -1,1 +1,10 @@
-consol.log('hello world');
+var http = require('http');
+console.log('hello world');
+
+// js listening on port 
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8124, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:8124/');
+
