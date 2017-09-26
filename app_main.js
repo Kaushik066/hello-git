d69663f1dbb645581a75c4ae7f1979b903a90953
Kaushik066 <masterkaushikrao@gmail.com>
2017-09-26 10:50:02 +0530

    this is new modification

--- app_main.js
+++ app_main.js
@@ -1,11 +1,4 @@
-var http = require('http');
+
 console.log('hello world');
 console.log('new changes made');
-
-// js listening on port 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
 
