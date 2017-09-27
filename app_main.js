0c1a434ac14c78c8fa59e48d324fffaae92d948c
Kaushik066 <masterkaushikrao@gmail.com>
2017-09-27 11:59:16 +0530

    Started all over again

--- app_main.js
+++ app_main.js
@@ -1,4 +1,13 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var app = express();
+app.use(morgan('combined'));
 
 console.log('hello world');
 console.log('new changes made');
 
+var port = 8123;
+app.listen(port, function () {
+  console.log(`hello-git app listening on port ${port}!`);
+});
