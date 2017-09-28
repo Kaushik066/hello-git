0b458d3487ba54b87abefb3636cb95d4070e39a6
Kaushik066 <masterkaushikrao@gmail.com>
2017-09-29 02:32:48 +0530

    source page added

--- app_main.js
+++ app_main.js
@@ -4,6 +4,14 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'ui','index.html'));
+});
+
+app.get('/ui/newStyle.css',function(req,res){
+  res.sendFile(path.join(__dirname,'ui','newStyle.css'));
+});
+
 console.log('hello world');
 console.log('new changes made');
 
